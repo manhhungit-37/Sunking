556c23330c96a73f0c7f772e4ddd18c881f62139
manhhungit-37 <manhhungit.37@gmail.com>
2023-05-25 02:09:28 +0700

    change script content

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,16 +10,14 @@ export default function Document() {
         <NextScript />
       </body>
       <div id="fb-root"></div>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `var chatbox = document.getElementById('fb-customer-chat');
+      <Script id="script-messager">
+        {`var chatbox = document.getElementById('fb-customer-chat');
         chatbox.setAttribute("page_id", "114354014986699");
-        chatbox.setAttribute("attribution", "biz_inbox");`}}
-      />
+        chatbox.setAttribute("attribution", "biz_inbox");`}
+      </Script>
 
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `window.fbAsyncInit = function() {
+      <Script id="script-fb">
+        {`window.fbAsyncInit = function() {
           FB.init({
             xfbml            : true,
             version          : 'v17.0'
@@ -33,8 +31,9 @@ export default function Document() {
           js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
           fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));
-        `}}
-      />
+        FB.CustomerChat.show()  
+        `}
+      </Script>
       <div id="fb-customer-chat" className="fb-customerchat">
       </div>
     </Html>

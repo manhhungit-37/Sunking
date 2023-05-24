dcbdc40d6c934a3b2c81a5d48ea82763328cd872
manhhungit-37 <manhhungit.37@gmail.com>
2023-05-25 01:31:37 +0700

    fix &~27 error

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,23 +9,22 @@ export default function Document() {
         <Main />
         <NextScript />
       </body>
-      <div id="fb-root"></div>
+    <div id="fb-root"></div>
 
-      <div id="fb-customer-chat" className="fb-customerchat">
-      </div>
+    <div id="fb-customer-chat" className="fb-customerchat">
+    </div>
 
-      <script>
-        {`var chatbox = document.getElementById('fb-customer-chat');
-      console.log(1);
+    <script>
+      var chatbox = document.getElementById('fb-customer-chat');
       chatbox.setAttribute("page_id", "114354014986699");
-      chatbox.setAttribute("attribution", "biz_inbox");`}
-      </script>
+      chatbox.setAttribute("attribution", "biz_inbox");
+    </script>
 
-      <script>
-        {`window.fbAsyncInit = function() {
+    <script>
+      window.fbAsyncInit = function() {
         FB.init({
           xfbml            : true,
-          version          : v17
+          version          : 'v17.0'
         });
       };
 
@@ -35,8 +34,8 @@ export default function Document() {
         js = d.createElement(s); js.id = id;
         js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
         fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));`}
-      </script>
+      }(document, 'script', 'facebook-jssdk'));
+    </script>
     </Html>
   );
 }

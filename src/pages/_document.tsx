5dd25d05411e2204f9828d2680ba987bc66b26d8
manhhungit-37 <manhhungit.37@gmail.com>
2023-05-25 00:13:04 +0700

    add facebook messager test

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,40 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html>
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
+      <div id="fb-root"></div>
+      <div id="fb-customer-chat" className="fb-customerchat">
+      </div>
+
+      <Script id="script-messager">
+        {`var chatbox = document.getElementById('fb-customer-chat');
+        chatbox.setAttribute("page_id", "114354014986699");
+        chatbox.setAttribute("attribution", "biz_inbox");`}
+      </Script>
+
+      <Script strategy="lazyOnload" id="script-fb">
+        {`window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v17.0'
+          });
+        };
+
+        (function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));`}
+      </Script>
     </Html>
   );
 }

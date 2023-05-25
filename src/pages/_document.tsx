bf10137c4ba7c856aaf220ce9b9df7935f6874b1
manhhungit-37 <manhhungit.37@gmail.com>
2023-05-26 00:49:51 +0700

    add script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,34 +8,36 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <div id="fb-root"></div>
+
+        <div id="fb-customer-chat" className="fb-customerchat">
+        </div>
+
+        <script>
+          var chatbox = document.getElementById('fb-customer-chat');
+          chatbox.setAttribute("page_id", "114354014986699");
+          chatbox.setAttribute("attribution", "biz_inbox");
+        </script>
+
+        <script>
+          window.fbAsyncInit = function() {
+            FB.init({
+              xfbml: true,
+              version: 'v17.0'
+            })
+          };
+
+          function run(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+            fjs.parentNode.insertBefore(js, fjs);
+          };
+          run(document, 'script', 'facebook-jssdk');
+        </script>
       </body>
-      <div id="fb-root"></div>
-      <Script id="script-messager">
-        {`var chatbox = document.getElementById('fb-customer-chat');
-        chatbox.setAttribute("page_id", "114354014986699");
-        chatbox.setAttribute("attribution", "biz_inbox");`}
-      </Script>
-
-      <Script id="script-fb">
-        {`window.fbAsyncInit = function() {
-          FB.init({
-            xfbml            : true,
-            version          : 'v17.0'
-          });
-        };
-
-        (function(d, s, id) {
-          var js, fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) return;
-          js = d.createElement(s); js.id = id;
-          js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
-          fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-        FB.CustomerChat.show()  
-        `}
-      </Script>
-      <div id="fb-customer-chat" className="fb-customerchat">
-      </div>
     </Html>
   );
 }

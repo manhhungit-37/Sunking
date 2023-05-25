e1e0c25c7a6987ce63c5e75266e9487e627324e4
manhhungit-37 <manhhungit.37@gmail.com>
2023-05-26 01:04:08 +0700

    change to test plugin facebook

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,29 +15,29 @@ export default function Document() {
           id="messenger-tag"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html: `{var chatbox = document.getElementById('fb-customer-chat');
-        chatbox.setAttribute("page_id", "114354014986699");
-        chatbox.setAttribute("attribution", "biz_inbox");`}}
+            __html: `var chatbox = document.getElementById('fb-customer-chat');
+            chatbox.setAttribute("page_id", "114354014986699");
+            chatbox.setAttribute("attribution", "biz_inbox");`}}
         />
         <Script
           id="messenger-sdk"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-        window.fbAsyncInit = function() {
-          FB.init({
-            xfbml: true,
-            version: 'v17.0'
-          })
-        };
-
-        (function(d, s, id) {
-          var js, fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) return;
-          js = d.createElement(s); js.id = id;
-          js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
-          fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));`}}
+            window.fbAsyncInit = function() {
+              FB.init({
+                xfbml            : true,
+                version          : 'v17.0'
+              });
+            };
+      
+            (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));`}}
         />
         <Main />
         <NextScript />
